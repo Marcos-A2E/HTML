6bcade554b807cb743af87effc466211a98e5624
Marcos Alexandre dos Santos 2ªE <164930251+Marcos-A2E@users.noreply.github.com>
2025-10-07 13:40:53 -0300

    Add files via upload

---
+++ S-24 front_end 3/portfolio/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.image').forEach(image => {
+    image.addEventListener('click', () => {
+        // Alterna a cor de fundo da imagem clicada
+        image.style.backgroundColor = image.style.backgroundColor === 'rgb(255, 99, 71)' ? '#ddd' : 'rgb(255, 99, 71)';
+    });
+});
